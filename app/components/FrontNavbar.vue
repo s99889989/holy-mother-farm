5f0abc626cc6cca6253c079369247687134015c9
s99889989 <s02048898067@gmail.com>
2026-05-22 21:12:02 +0800

    2026/05/22-21:11 更新

--- app/components/FrontNavbar.vue
+++ app/components/FrontNavbar.vue
@@ -423,15 +423,15 @@ onUnmounted(() => {
                   帳號設定
                 </NuxtLink>
               </li>
-<!--              <li v-if="canAccessStaff" class="avatar-dropdown__divider">-->
-<!--                <NuxtLink to="/staff/home" @click="closeAvatar" class="avatar-dropdown__item avatar-dropdown__item&#45;&#45;staff">-->
-<!--                  <svg width="18" height="18" fill="none" stroke="currentColor" viewBox="0 0 24 24">-->
-<!--                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
-<!--                          d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-2 10v-5a1 1 0 00-1-1h-2a1 1 0 00-1 1v5m4 0H9"/>-->
-<!--                  </svg>-->
-<!--                  員工專區-->
-<!--                </NuxtLink>-->
-<!--              </li>-->
+              <li v-if="canAccessStaff" class="avatar-dropdown__divider">
+                <NuxtLink to="/staff/home" @click="closeAvatar" class="avatar-dropdown__item avatar-dropdown__item--staff">
+                  <svg width="18" height="18" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-2 10v-5a1 1 0 00-1-1h-2a1 1 0 00-1 1v5m4 0H9"/>
+                  </svg>
+                  員工專區
+                </NuxtLink>
+              </li>
               <li class="avatar-dropdown__divider">
                 <button @click="logout" class="avatar-dropdown__logout">
                   <svg width="18" height="18" fill="none" stroke="currentColor" viewBox="0 0 24 24">
