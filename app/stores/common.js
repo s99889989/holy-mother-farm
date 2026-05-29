a9681c426c1f13dfeff1fb5847cc528d065c8696
s99889989 <s02048898067@gmail.com>
2026-05-29 17:55:42 +0800

    2026/05/29-17:55 更新

--- app/stores/common.js
+++ app/stores/common.js
@@ -4,10 +4,10 @@ export const useCommonStore = defineStore('useFamilyStore', () => {
   // https://madustrialtd.asuscomm.com:9100
   // http://localhost:9100
   // https://madustrialtd.asuscomm.com:8080
-  // https://api.karltw.com
+  // https://api.karltw.com:8080
   // http://localhost:8080
   const data = reactive({
-    main_url: 'https://api.karltw.com',
+    main_url: 'https://api.karltw.com:8080',
     google_client_id: '441605672654-9j73r51g6j2mar17ptblhskfvard1em9.apps.googleusercontent.com'
   })
   return { data }
