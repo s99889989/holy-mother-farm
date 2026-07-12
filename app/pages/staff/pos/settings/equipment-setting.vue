eb0c350a2821fbf02f0144b18fab4956c94a92f5
s99889989 <s02048898067@gmail.com>
2026-07-13 07:28:09 +0800

    2026/07/13-07:28 更新

--- app/pages/staff/pos/settings/equipment-setting.vue
+++ app/pages/staff/pos/settings/equipment-setting.vue
@@ -3,7 +3,10 @@ definePageMeta({ layout: 'staff', requiredPermission: 'pos.settings.equipment-se
 </script>
 
 <template>
-
+  <div class="p-4">
+    <h1 class="text-xl font-bold">銷售分析</h1>
+    <p class="text-gray-500 mt-2">此頁面尚在建置中</p>
+  </div>
 </template>
 
 <style scoped>
