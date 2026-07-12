ccab3c092f4cbec4f1205856f4bc3c9d5caaecde
s99889989 <s02048898067@gmail.com>
2026-07-13 07:26:48 +0800

    2026/07/13-07:26 更新

--- app/pages/staff/pos/analyze/sales-analysis.vue
+++ app/pages/staff/pos/analyze/sales-analysis.vue
@@ -1,10 +1,13 @@
-<script lang="ts">
+<script setup lang="ts">
 definePageMeta({ layout: 'staff', requiredPermission: 'pos.analyze.pos-sales' })
 </script>
 
 <template>
+  <div class="p-4">
+    <h1 class="text-xl font-bold">銷售分析</h1>
+    <p class="text-gray-500 mt-2">此頁面尚在建置中</p>
+  </div>
 </template>
 
 <style scoped>
-
 </style>
