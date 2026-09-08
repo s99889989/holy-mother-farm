ad27655ab59d1407dfe7c07da8934047429e3b46
s99889989 <s02048898067@gmail.com>
2026-09-08 12:26:50 +0800

    2026/09/08-12:26

--- app/components/StaffNavbar.vue
+++ app/components/StaffNavbar.vue
@@ -75,7 +75,7 @@ const navGroups = [
       { to: '/staff/content/front-website', icon: '📢️', label: '前台管理', key: 'content.front-website' },
       { to: '/staff/content/tour', icon: '🌐️', label: '環景導覽管理', key: 'content.tour' },
       { to: '/staff/content/files', icon: '📁', label: '檔案管理', key: 'content.files' },
-      { to: '/staff/content/gaussian-models', icon: '📁', label: '高斯潑灑模型', key: 'content.files' }
+      { to: '/staff/content/gaussian-models', icon: '📁', label: '高斯潑灑模型', key: 'content.gaussian-models' }
     ]
   },
   {
